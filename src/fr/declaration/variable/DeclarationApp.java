8eeb4b2658df477b4cb25ee2dcad49cd1ef2ff38
Kevin Le Heurt <leheurt.kevin@gmail.com>
2021-12-09 13:55:58 +0100

    1er commit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,25 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		byte by = 11;
+		short s = 12;
+		int i = 13;
+		long l = 14L;
+		float f = 1.5F;
+		double d = 1.6;
+		char c = 'c';
+		boolean bo = true;
+		String str = "chaine";
+		System.out.println(str);
+		
+		String randomString = "Voici le résultat d’un calcul : 1+5=6";
+		System.out.println(randomString);
+		randomString =  randomString.substring(0,31)+"\n"+randomString.substring(31);
+		System.out.println(randomString);
+
+		
+	}
+
+}
