bb9607c1c3b364844a4ab3380d67e57be47d2513
Kevin Le Heurt <leheurt.kevin@gmail.com>
2021-12-09 13:55:58 +0100

    2e commit

---
+++ src/fr/boucles/ExerciceBoucleBase.java
@@ -0,0 +1,11 @@
+package fr.boucles;
+
+public class ExerciceBoucleBase {
+	
+	public void afficherNombres() {
+		for(int i = 1 ; i <= 10 ; i++) {
+			System.out.println(i);
+		}
+	}
+	
+}
---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,30 @@
+package fr.declaration.variable;
+import fr.boucles.ExerciceBoucleBase;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		byte by = 11;
+		short s = 12;
+		int i = 13;
+		long l = 14L;
+		float f = 1.5F;
+		double d = 1.6;
+		char c = 'c';
+		boolean bo = true;
+		String str = "chaine";
+		System.out.println(str);
+		
+		String randomString = "Voici le résultat d’un calcul : 1+5=6";
+		System.out.println(randomString);
+		randomString =  randomString.substring(0,31)+"\n"+randomString.substring(31);
+		System.out.println(randomString);
+		
+		
+		ExerciceBoucleBase e1 = new ExerciceBoucleBase();
+		e1.afficherNombres();
+
+		
+	}
+
+}
